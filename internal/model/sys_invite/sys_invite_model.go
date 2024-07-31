f22e0991a57e60ee7fcce6df2c0f8774d6ebf00f
huof6829 <68298506@qq.com>
2024-07-31 13:12:01 +0000

    rm no use func

--- internal/model/sys_invite/sys_invite_model.go
+++ internal/model/sys_invite/sys_invite_model.go
@@ -3,7 +3,6 @@ package sys_invite
 import (
 	"context"
 
-	"github.com/SpectatorNan/gorm-zero/gormc"
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -24,7 +23,6 @@ type (
 	}
 
 	customSysInviteLogicModel interface {
-		FindOneByChildId(ctx context.Context, tx *gorm.DB, childId int64) (*SysInvite, error)
 		InsertOnUpdate(ctx context.Context, tx *gorm.DB, data *SysInvite) error
 	}
 )
@@ -47,27 +45,6 @@ func (m *defaultSysInviteModel) customCacheKeys(data *SysInvite) []string {
 		return []string{}
 	}
 	return []string{}
-}
-
-func (m *customSysInviteModel) FindOneByChildId(ctx context.Context, tx *gorm.DB, childId int64) (*SysInvite, error) {
-	var resp SysInvite
-	err := m.ExecNoCache(func(conn *gorm.DB) error {
-		db := conn
-		if tx != nil {
-			db = tx
-		}
-		return db.WithContext(ctx).
-			Where("child_id = ?", childId).
-			Find(&resp).Error
-	})
-	switch err {
-	case nil:
-		return &resp, nil
-	case gormc.ErrNotFound:
-		return nil, ErrNotFound
-	default:
-		return nil, err
-	}
 }
 
 func (m *customSysInviteModel) InsertOnUpdate(ctx context.Context, tx *gorm.DB, data *SysInvite) error {
